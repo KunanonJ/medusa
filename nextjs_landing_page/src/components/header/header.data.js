f5baa6ef735236071d7934d46f06f9ebb7b6fcfe
Kunanon <fronkzoza001@gmail.com>
2021-10-11 19:43:56 +0700

    ui updated

---
+++ nextjs_landing_page/src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'What is ZapBlink',
+  },
+  {
+    path: 'Whitepaper',
+    label: 'Whitepaper',
+  },
+  {
+    path: 'RoadMap',
+    label: 'Roadmap',
+  },
+  {
+    path: 'TokenDistribution',
+    label: 'Token Distribution',
+  },
+];
